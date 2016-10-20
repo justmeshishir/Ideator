d3e76cf6504947a7fb8a0998e53e2b3337ac352e
Shishir Thapa <shishirthapa1@gmail.com>
2016-10-20 07:46:05 +0000

    settong up routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   root 'ideas#index'
   
   resources :ideas
+  
+  get 'about' => 'static_pages#about'
+  get 'random' => 'static_pages#random'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
