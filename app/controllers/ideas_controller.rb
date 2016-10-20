c5a4d145ebf46b1c1588248d26c5c433969fa5c9
Shishir Thapa <shishirthapa1@gmail.com>
2016-10-20 07:07:35 +0000

    Validating

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,6 +5,11 @@ class IdeasController < ApplicationController
   
   def create
     @idea = Idea.create(idea_params)
+    if @ideas.valid?
+      
+    else
+    
+    end
     redirect_to root_path
   end
   
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,3 @@
 class Idea < ActiveRecord::Base
+    validates :content, :author, presence: true
 end
