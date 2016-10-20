2457799c3ec3bc58f4a955031141077a7e632966
Shishir Thapa <shishirthapa1@gmail.com>
2016-10-20 09:00:17 +0000

    adding flash

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,6 +5,10 @@ class IdeasController < ApplicationController
   
   def create
     @idea = Idea.create(idea_params)
+    if @idea.valid?
+      flash[:success]= "Your idea has been posted!"
+    else
+      flash[:alert]= "Woops! Looks like there has been an error!"
     redirect_to root_path
   end
   
@@ -15,8 +19,10 @@ class IdeasController < ApplicationController
   def update
     @idea = Idea.find(params[:id])
     if @idea.update(idea_params)
+      flash[:success] = "The idea has been updated!"
       redirect_to root_path
     else
+      flash[:alert] = "Woops! Looks like there has been an error!"
       redirect_to edit_idea_path(params[:id])
     end
   end
@@ -24,6 +30,7 @@ class IdeasController < ApplicationController
   def destroy
     @idea = Idea.find(params[:id])
     @idea.destroy
+    flash[:success] = "The idea was successfully deleted!"
     redirect_to root_path
   end
   private
